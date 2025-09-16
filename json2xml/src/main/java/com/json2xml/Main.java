49d0c9029b45ca03eeeb1469b64652d121efd934
axelsson <aa255tr@student.lnu.se>
2025-09-16 17:18:13 +0200

    Changed project from using no build tools, to using maven. As well as using Gson as JSON parser to Java objects.

---
+++ json2xml/src/main/java/com/json2xml/Main.java
@@ -0,0 +1,9 @@
+package com.json2xml;
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // Gson test = 
+    }
+}
