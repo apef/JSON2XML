ed9e5b76dd652f4b9519d8e9295227498d327065
axelsson <aa255tr@student.lnu.se>
2025-09-16 21:57:51 +0200

    Simple support for writing XML added using a Stringbuilder and manual string writing.

--- json2xml/src/main/java/com/json2xml/Main.java
+++ json2xml/src/main/java/com/json2xml/Main.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Set;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -28,6 +30,7 @@ public class Main {
       System.err.println("JSON file could not be found, check the filepath.");
     } else {
       String XML = parser(jsonStr);
+      System.out.println("\n\n\n\n" + XML);
     }
   }
 
@@ -57,7 +60,9 @@ public class Main {
       System.out.println("Size less than one");
       sb.append("<Geometries/>");
     } else {
+      sb.append("<Geometries>");
       for (JsonElement jsonElement : features) {
+        sb.append("<Geometry>");
         JsonObject feature = jsonElement.getAsJsonObject();
         JsonObject attributes = feature.getAsJsonObject("attributes");
         JsonObject geometry = feature.getAsJsonObject("geometry");
@@ -67,15 +72,44 @@ public class Main {
         String vaghallare = getAttributeAsString(attributes, "Vaghallare");
         
         String id = getAttributeAsString(attributes, "ID");
-        id = (id == null) ? "ID Saknas" : id;
+        id = (id == null) ? "ID saknas" : id;
   
         String X = getAttributeAsString(geometry, "x");
         String Y = getAttributeAsString(geometry, "y");
-        
+        String XY = "POINT(" + X + " " + Y + ")";
+        sb.append(writeXMLAttribute("ObjectID", objID));
+        sb.append(writeXMLAttribute("Name", id));
+        sb.append(writeXMLAttribute("Description", "Ägare: " + owner));
+        // sb.append(writeXMLAttribute(vaghallare, json))
+        sb.append(writeXMLAttribute("WKTGeometry", XY));
+        String selectable = isSelectable(owner) ? "true" : "false";
+        sb.append(writeXMLAttribute("Selectable", selectable));
+        sb.append(writeXMLAttribute("IconURL", "https://minasidor.testkommun.se/fileconnector/file/cesamh/456_felanmalan_belysning/trbel_normal_light_green_16px.png"));
+
+        // <Geometry>
+        //   <ObjectID>2227910</ObjectID>
+        //   <Name>7910</Name>
+        //   <Description>Ägare: Kalmar Energi</Description>
+        //   <WKTGeometry>POINT(140767.4570000004 6287207.636700001)</WKTGeometry>
+        //   <Selectable>true</Selectable>
+        //   <IconURL>https://minasidor.testkommun.se/fileconnector/file/cesamh/456_felanmalan_belysning/trbel_normal_light_green_16px.png</IconURL>
+        // </Geometry>
+
+        sb.append("</Geometry>");
       }
+      sb.append("</Geometries>");
     }
 
     return sb.toString();
+  }
+
+  private static boolean isSelectable(String name) {
+    // String[] invalidNames = {"Kalmar kommun - Fastighetsservice", "Trafikverket"};
+    Set<String> invalidNames = Set.of("Kalmar kommun - Fastighetsservice", "Trafikverket");
+    if (!invalidNames.contains(name) && !(name.contains("GA:".toLowerCase()))) {
+      return true;
+    }
+    return false;
   }
 
   private static String getAttributeAsString(JsonObject object, String value) {
@@ -89,6 +123,10 @@ public class Main {
     return returnValue;
   }
 
+  public static String writeXMLAttribute(String tagName, String value) {
+    // <ObjectID>2227910</ObjectID>
+    return "<" + tagName + ">" + value + "</" + tagName + ">";
+  }
   // <Geometries/> if none
 
   /**
