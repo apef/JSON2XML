d6794cd0d9932e0e45b0e3e0be5f9f49ec16e6c7
axelsson <aa255tr@student.lnu.se>
2025-09-16 19:15:55 +0200

    Initial set up done with Maven. Reading JSON file into a string, which can be sent to the parser.

--- json2xml/src/main/java/com/json2xml/Main.java
+++ json2xml/src/main/java/com/json2xml/Main.java
@@ -1,9 +1,30 @@
 package com.json2xml;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import com.google.gson.Gson;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        // Gson test = 
+  public static void main(String[] args) throws IOException {
+    String jsonStr = null;
+    try (InputStream in = Main.class.getResourceAsStream("/RAWDATA.JSON"))  {
+      if (in == null) {
+        throw new FileNotFoundException("JSON File could not be found on the classpath");
+      } else {
+        jsonStr = new String(in.readAllBytes());
+      }
     }
+
+    if (jsonStr.equals(null)) {
+      System.err.println("JSON file could not be found, check the filepath.");
+    } else {
+      File XML = parser(jsonStr);
+    }
+  }
+
+  public static File parser(String json) {
+    return null;
+  }
 }
